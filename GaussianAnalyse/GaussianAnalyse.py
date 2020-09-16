009c4fd4218af18edb5ca14675fdb73e745a78e2
Mikhail Ivanov <54251259+mikhail-a-ivanov@users.noreply.github.com>
2020-09-16 14:14:29 +0200

    Delete GaussianAnalyse.py

--- GaussianAnalyse/GaussianAnalyse.py
+++
@@ -1,7 +0,0 @@
-from GaussianAnalyseFunctions import findGaussianOutput, writePerformanceStats, writeEnergyStats
-
-output_filenames = findGaussianOutput()
-
-#writePerformanceStats(output_filenames, csv_name='performance.csv')
-
-writeEnergyStats(output_filenames, csv_name='energy.csv')
